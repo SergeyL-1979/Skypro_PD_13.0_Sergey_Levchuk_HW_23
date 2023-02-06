0b00f52a5443e3b613c14aa7e12804e38f918785
sergd <videomc1979@gmail.com>
2023-02-06 16:12:21 +0300

    add

--- views.py
+++ views.py
@@ -11,11 +11,6 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/perform_query", methods=['POST'])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    # return app.response_class('', content_type="text/plain")
     data = request.json
 
     try:
